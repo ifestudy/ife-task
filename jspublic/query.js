5b0b5ef497b6fc3a50a92c763a44f50d35cb3270
iceprosurface <775846939@qq.com>
2016-07-01 22:29:19 +0800

    新增css操作，增加一些注释
    新增了css，addClass，removeClass的方法

--- jspublic/query.js
+++ jspublic/query.js
@@ -1,9 +1,11 @@
 (function() {
     "use strict";
-    // 用于保存整个
+    //确保不被重复加载
     var _$ = window.$,
+        // 用于保存整个
         rootQuery = [],
         _$ = function(selector){
+        //生成器每一次返回一个新的对象
         return new _$.fn.init(selector,rootQuery);
     };
 
@@ -16,11 +18,16 @@
     */
     _$.fn = _$.prototype = {
         version: "0.0.1",
+        //用于判断是否为query对象
         isQuery: true,
         constructor: _$,
+        //默认为空
         length : 0,
+        //继承splice方法
         splice: [].splice,
+        //选择器暂留器（暂不使用）
         selector : '',
+        //对象是否是json型数据（object和array也会被认为是json）
         isJson : false,
         init: function(selector,rootQuery){
             var quickCheck = /^(?=(#[\w-]+$)|([.][\w-]+$)|((?:[#|.][\w-]+[\s]+)+(?:[#|.][\w-]*)$))/g;
@@ -38,7 +45,7 @@
             // $3:一个多级列表
             // $4:预留一个tag检测位置
             // $5:预留一个~
-
+            //保留做兼容！
             //匹配到的情况下
             // if(matchs){
             //     //id类
@@ -73,13 +80,13 @@
                 }
                 return this;
             }
-            //
-            if ( selector !== undefined ) {}
-            // 数组和object类别(不做深拷贝处理)
+            //预留他用！
+            // if ( selector !== undefined ) {}
+            // 数组和object类别(不做深拷贝处理)统一认为做json型处理！
             if ( (typeof selector == 'array') || (typeof selector == 'object') ||((typeof elem == 'object')&&(Object.prototype.toString.call(elem).toLowerCase())=="[object object]"&&!elem.length) ) {
                 var elem = selector;
                 this.selector = elem.selector;
-                this[0] = elem
+                this[0] = elem;
                 this.length = elem.length;
                 this.isJson = true;
                 return this;
@@ -96,18 +103,66 @@
                 this.length = elem.length;
                 return this;
             }
-            //开始判断
+            //保留selector（不确定出现这种情况的selector是什么样子的，保留做debug）
             this.selector = selector;
+            //如果什么都不是则返回空！以防止出错
+            this.length=0;
             return this;
         },
         //class
+        css : function( attr , css ){
+            switch(arguments.length){
+                case 1: 
+                    //首先判断是否是json数据，是的话那就依次设置（设置）
+                    if((typeof attr == 'object')&&(Object.prototype.toString.call(attr).toLowerCase())=="[object object]"&&!attr.length){
+                        //遍历自己
+                        this.each(function(i,item){ 
+                            //遍历元素
+                            $(attr).each(function(j,attrd){
+                                item.style[j] = attrd;
+                            });
+                        });
+                        //结束返回自己
+                        return this;
+                    }else{
+                        // this.each(function(i,item){ 
+                        return this[0].style[attr];
+                    }
+                    break;
+                case 2:
+                    //如果有两个参数那必然是遍历设置
+                    this.each(function(i,item){ 
+                        item.style[attr] = css;
+                    });
+                    return this;
+                    break;
+                default: 
+                    return "";
+            }
+        },
         addClass : function( ClassName ) {
-
+            if((this[0].nodeType)&&(this[0].nodeType != 11)){
+                $(this[0]).each(function(i,item){
+                    if(!item.className.match(new RegExp("(\\s|^)"+ClassName+"(\\s|$)","g"))){
+                        item.className = ClassName;
+                    }
+                });
+                return this;
+            }else{
+                return this;
+            }
         },
         removeClass : function( ClassName ) {
-
+            if((this[0].nodeType)&&(this[0].nodeType != 11)){
+                $(this[0]).each(function(i,item){
+                    item.className = item.className.replace(new RegExp("(\\s|^)"+ClassName+"(\\s|$)","g"),"");
+                });
+                return this;
+            }else{
+                return this;
+            }
         },
-        //dom操作
+        //dom操作！TODO:采用了讨巧的办法，不合时宜！
         find : function(selector){
             // tsk;
         },
@@ -156,7 +211,9 @@
         getPosition : function() {
 
         },
+        //事件侦听！
         on : function( name,listener ) {
+            //预留检测位置！
             if( isNaN(this.length) || (this.length < 0) ){
                 return false;
             //     this[0].addEventListener(name,listener);
