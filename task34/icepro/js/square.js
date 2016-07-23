0253d4092dd786f2d25382fdb85637045937669e
iceprosurface <775846939@qq.com>
2016-07-23 15:13:30 +0800

    task34完成哈哈
    easy~扩展性真是太好啦哈哈哈哈

---
+++ task34/icepro/js/square.js
@@ -0,0 +1,12 @@
+(function($,window){
+	//该模块是下面所有模块的总类，定义一些常量和访问接口
+	class square {
+		constructor( editorDom , ctx , mapSize) {
+			this.editor = new this.editor(editorDom);
+			this.map = new this.map(mapSize);
+			this.robot = new this.robot(this.map);
+			this.rander = new this.rander(ctx,this.map,this.robot);
+		}
+	}
+	window.square = square;
+})($,window);
